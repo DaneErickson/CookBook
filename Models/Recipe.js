043edf958d2c8ab9d9752688a867a892aae6f117
Dane Erickson <danemit@mit.edu>
2016-05-20 10:11:21 -0400

    added populate step to model

--- Models/Recipe.js
+++ Models/Recipe.js
@@ -8,7 +8,8 @@ const stepSchema       = require('./Step.js').Schema;
 const ingredientModel  = require('./Ingredient.js').Model;
 const nutritionModel   = require('./Nutrition.js').Model;
 
-
+// A document mapping an author, a list of steps, and a list of ingredients
+// to the abstract idea of a recipe.
 const recipeSchema = mongoose.Schema({
   author      : {
                   name   : String,
@@ -27,18 +28,35 @@ const recipeSchema = mongoose.Schema({
                 }],
 });
 
-// returns a json object representing all the nutrients in the recipe.
+// returns a Nutrition document representing all of the nutrients in the recipe.
 // TODO improvements: divide & conquer, recursively sum over left half,
 //                    over right half, then combine.
 recipeSchema.methods.getNutrients = function(){
   if (this.ingredients.length === 0){
     return new ingredientModel({});
   }
+  this.populate('ingredients');
   var nutrients = this.ingredients[0].getNutrients(this.ingredients[0].amount);
   for(var i = 1; i<this.ingredients.length; i++){
     nutrients = nutritionModel.sum(nutrients,this.ingredients[i]);
   }
   return nutrients;
+}
+
+// param step: a Step document
+// adds the Step to the list of steps in the recipe
+recipeSchema.methods.addStep = function(step){
+  this.steps.push(step);
+}
+
+// param ingredient: an Ingredient document
+// param amount    : the amount of the ingredient to add.
+// adds the Ingredient to the list of ingredients in the recipe.
+recipeSchema.methods.addIngredient = function(ingredient,amount){
+  this.ingredients.push({
+    ingredient : ingredient,
+    amount     : amount,
+  });
 }
 
 module.exports = mongoose.model('Recipe', recipeSchema);
