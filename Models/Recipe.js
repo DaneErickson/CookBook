ca49db044c89327dc8c39297b2c70219064e731e
Dane Erickson <danemit@mit.edu>
2016-05-20 09:46:46 -0400

    reformatting

--- Models/Recipe.js
+++ Models/Recipe.js
@@ -11,18 +11,18 @@ const nutritionModel   = require('./Nutrition.js').Model;
 
 const recipeSchema = mongoose.Schema({
   author      : {
-                    name   : String,
-                    email  : String,
+                  name   : String,
+                  email  : String,
                 },
   steps       : [{
-                    type : Schema.Types.ObjectId,
-                    ref  : 'Step',
+                  type : Schema.Types.ObjectId,
+                  ref  : 'Step',
                 }],
   ingredients : [{ingredient : {
                     type : Schema.Types.ObjectId,
                     ref  : 'Ingredient',
                   },
-                  amount: { quantity: { type: Number, default: 0 },
+                  amount : { quantity: { type: Number, default: 0 },
                             unit    : { type: String, default: 'g' }},
                 }],
 });
